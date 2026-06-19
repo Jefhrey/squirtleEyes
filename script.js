84f7fdbb245863676f5bc0f1e5665b16ffc4ac46
Jefhrey <jefhrey01@gmail.com>
2026-06-19 20:54:07 +0530

    Fix: fully working swivel style eyes

--- script.js
+++ script.js
@@ -1,61 +1,85 @@
-const wrapper = document.querySelector(".wrapper");
-const HEIGHT = document.querySelector("body").offsetHeight;
-const WIDTH = document.querySelector("body").offsetWidth;
 
-const leftX = [54, 63];
-const leftY = [18, 29];
+// const leftX = [54, 63];
+// const leftY = [18, 29];
 
-const rightX = [6.5, 20];
-const rightY = [16.5, 31.5];
+// const rightX = [6.5, 20];
+// const rightY = [16.5, 31.5];
 // Origin -> the center of the png.
+
+
 document.addEventListener("DOMContentLoaded", () => {
+    
+const wrapper = document.querySelector(".wrapper");
+const HEIGHT = window.innerHeight;
+const WIDTH = window.innerWidth;
+
+const rect = wrapper.getBoundingClientRect();
+const wrapperX = rect["x"];
+const wrapperY = rect["y"];
+const imgHeight = rect["height"];
+const imgWidth = rect["width"];
+
+const centerX =  wrapperX + (imgWidth/2);
+const centerY =  wrapperY + (imgHeight/2);
 
     document.addEventListener("mousemove", (e) =>{
-        x = e.clientX / WIDTH;
-        y = e.clientY / HEIGHT;
+        // x = e.clientX / WIDTH;
+        // y = e.clientY / HEIGHT;
     
-        coordLeftX = 63 - (x * 9);
-        coordLeftY = (y * 11) + 18;
+        // coordLeftX = 63 - (x * 9);
+        // coordLeftY = (y * 11) + 18;
 
-        coordRightX = 20 - (x * 13.5);
-        coordRightY = (y * 15) + 16.5;
-        moveLeftX(coordLeftX);
-        moveLeftY(coordLeftY);
+        // coordRightX = 20 - (x * 13.5);
+        // coordRightY = (y * 15) + 16.5;
+        // moveLeftX(coordLeftX);
+        // moveLeftY(coordLeftY);
 
-        moveRightX(coordRightX);
-        moveRightY(coordRightY);
+        // moveRightX(coordRightX);
+        // moveRightY(coordRightY);
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        const wrapperRelativeX = mouseX - centerX;
+        const wrapperRelativeY = mouseY - centerY;
+
+        let radianAngle = Math.atan2(wrapperRelativeY, wrapperRelativeX);
+        if (radianAngle < 0)
+            radianAngle += Math.PI*2;
+        let degAngle = (radianAngle * 180) / Math.PI;
+        degAngle += 45;
+        wrapper.style.setProperty("--rotation-angle", `${degAngle}deg`); 
     })
 })
 
 
-function moveLeftX(coord)
-{
-    wrapper.style.setProperty(
-        "--left-eye-x",
-        `${coord}%`
-    );
-}
+// function moveLeftX(coord)
+// {
+//     wrapper.style.setProperty(
+//         "--left-eye-x",
+//         `${coord}%`
+//     );
+// }
 
-function moveLeftY(coord)
-{
-    wrapper.style.setProperty(
-        "--left-eye-y",
-        `${coord}%`
-    );
-}
+// function moveLeftY(coord)
+// {
+//     wrapper.style.setProperty(
+//         "--left-eye-y",
+//         `${coord}%`
+//     );
+// }
 
-function moveRightX(coord)
-{
-    wrapper.style.setProperty(
-        "--right-eye-x",
-        `${coord}%`
-    );
-}
+// function moveRightX(coord)
+// {
+//     wrapper.style.setProperty(
+//         "--right-eye-x",
+//         `${coord}%`
+//     );
+// }
 
-function moveRightY(coord)
-{
-    wrapper.style.setProperty(
-        "--right-eye-y",
-        `${coord}%`
-    );
-}
+// function moveRightY(coord)
+// {
+//     wrapper.style.setProperty(
+//         "--right-eye-y",
+//         `${coord}%`
+//     );
+// }
